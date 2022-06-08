dd5c570cfa39deb22d2bd2f94315f7fa1db70180
kyk1778 <85525096+kyk1778@users.noreply.github.com>
2022-06-08 20:40:09 +0900

    [update]
    qna, 이벤트 글 작성, 목록 보기, 상세글 조회 구현

--- FiT_RPRJ/src/main/java/com/fit/model/EventVO.java
+++ FiT_RPRJ/src/main/java/com/fit/model/EventVO.java
@@ -1,17 +1,98 @@
 package com.fit.model;
 
-import java.util.Date;
-import java.util.List;
+import java.sql.Date;
 
-import lombok.Data;
-
-@Data
 public class EventVO {
-	private String aid;
-	private String title;
-	private Long bno;
-	private String content;
-	private Date reg;
-	private int hit;
-	private List<BoardAttachVO> bannerList;
+
+	//占쌜쇽옙占쏙옙(占쏙옙占쏙옙占쏙옙)
+	private String aId;
+	
+	private String aName;
+	
+	//占쏙옙膀濱占� 占쏙옙 占쌘듸옙占쏙옙 占쏙옙호
+	private String eTitle;
+	
+	//占쏙옙肪聘占쏙옙占�
+	private String eContent;
+	
+	private Date eReg;
+	
+	private int eBno;
+	
+	private int eHit;
+	
+	private int eFile;
+
+	public String getaId() {
+		return aId;
+	}
+
+	public void setaId(String aId) {
+		this.aId = aId;
+	}
+
+	public String getaName() {
+		return aName;
+	}
+
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+
+	public String geteTitle() {
+		return eTitle;
+	}
+
+	public void seteTitle(String eTitle) {
+		this.eTitle = eTitle;
+	}
+
+	public String geteContent() {
+		return eContent;
+	}
+
+	public void seteContent(String eContent) {
+		this.eContent = eContent;
+	}
+
+	public Date geteReg() {
+		return eReg;
+	}
+
+	public void seteReg(Date eReg) {
+		this.eReg = eReg;
+	}
+
+	public int geteBno() {
+		return eBno;
+	}
+
+	public void seteBno(int eBno) {
+		this.eBno = eBno;
+	}
+
+	public int geteHit() {
+		return eHit;
+	}
+
+	public void seteHit(int eHit) {
+		this.eHit = eHit;
+	}
+
+	public int geteFile() {
+		return eFile;
+	}
+
+	public void seteFile(int eFile) {
+		this.eFile = eFile;
+	}
+
+	@Override
+	public String toString() {
+		return "EventVO [aId=" + aId + ", aName=" + aName + ", eTitle=" + eTitle + ", eContent=" + eContent + ", eReg="
+				+ eReg + ", eBno=" + eBno + ", eHit=" + eHit + ", eFile=" + eFile + "]";
+	}
+
+	
 }
+	
---
+++ FiT_RPRJ/src/main/java/com/fit/model/QnaVO.java
@@ -0,0 +1,135 @@
+package com.fit.model;
+
+import java.sql.Date;
+
+public class QnaVO {
+
+	//작성자(관리자 아이디)
+	private String aId;
+
+	
+	//작성자(사용자 아이디)
+	private String userId;
+	
+	//작성자 (관리자 이름)
+	private String aName;
+	
+	//작성자 (사용자 이름)
+	private String userName;
+	
+	//문의 게시판 제목
+	private String qTitle;
+	
+	//문의 게시판 내용
+	private String qContent;
+	
+	//작성일
+	private Date qReg;
+	
+	//게시판 번호
+	private int qBno;
+	
+	
+	//조회수
+	private int qHit;
+
+
+	public String getaId() {
+		return aId;
+	}
+
+
+	public void setaId(String aId) {
+		this.aId = aId;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getaName() {
+		return aName;
+	}
+
+
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getqTitle() {
+		return qTitle;
+	}
+
+
+	public void setqTitle(String qTitle) {
+		this.qTitle = qTitle;
+	}
+
+
+	public String getqContent() {
+		return qContent;
+	}
+
+
+	public void setqContent(String qContent) {
+		this.qContent = qContent;
+	}
+
+
+	public Date getqReg() {
+		return qReg;
+	}
+
+
+	public void setqReg(Date qReg) {
+		this.qReg = qReg;
+	}
+
+
+	public int getqBno() {
+		return qBno;
+	}
+
+
+	public void setqBno(int qBno) {
+		this.qBno = qBno;
+	}
+
+
+	public int getqHit() {
+		return qHit;
+	}
+
+
+	public void setqHit(int qHit) {
+		this.qHit = qHit;
+	}
+
+
+	@Override
+	public String toString() {
+		return "QnaVO [aId=" + aId + ", userId=" + userId + ", aName=" + aName + ", userName=" + userName + ", qTitle="
+				+ qTitle + ", qContent=" + qContent + ", qReg=" + qReg + ", qBno=" + qBno + ", qHit=" + qHit + "]";
+	}
+	
+	
+	
+}
