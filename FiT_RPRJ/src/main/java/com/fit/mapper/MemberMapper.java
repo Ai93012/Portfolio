54bc1db5a25121e675c12ee505af1ed891a720c8
kyk1778 <85525096+kyk1778@users.noreply.github.com>
2022-06-14 21:41:26 +0900

    update
    회원 아이디 찾기 구현

--- FiT_RPRJ/src/main/java/com/fit/mapper/MemberMapper.java
+++ FiT_RPRJ/src/main/java/com/fit/mapper/MemberMapper.java
@@ -1,18 +1,21 @@
 package com.fit.mapper;
 
+import java.util.List;
+
 import com.fit.model.MemberVO;
 
 public interface MemberMapper {
 
+	
 	//회원가입
-		public void memberJoin(MemberVO member);
+	public void memberJoin(MemberVO member);
 
 	
 	//로그인
-		public MemberVO memberLogin(MemberVO member);
+	public MemberVO memberLogin(MemberVO member);
 		
 	//아이디 중복 검사
-		public int idCheck(String userId);
+	public int idCheck(String userId);
 		
 	//패스워드 체크
 	public int passCheck(String userPass) throws Exception;
@@ -20,12 +23,9 @@ public interface MemberMapper {
 	/* 회원 정보 수정*/
 	public void memberUpdate(MemberVO member);
 	
-
-	
-	/*회원 탈퇴 */
+	//회원 탈퇴
 	public void memberDelete(MemberVO member);
-
-		
+	
 		
 		
 }
