f92646af8dc83fc27140bb194b56ecab96b9d0a9
kyk1778 <85525096+kyk1778@users.noreply.github.com>
2022-06-14 21:41:50 +0900

    update
    회원 아이디 구현(미완)

---
+++ MemberServiceImpl.java
@@ -0,0 +1,73 @@
+
+package com.fit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fit.mapper.MemberMapper;
+import com.fit.model.MemberVO;
+
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+public class MemberServiceImpl implements MemberService{
+
+	@Autowired
+	private MemberMapper membermapper;
+
+	
+	/* ȸ������ */
+	@Override
+	public void memberJoin(MemberVO member) throws Exception {
+		
+		membermapper.memberJoin(member);
+		
+	}
+	
+	/* ���̵� �ߺ� Ȯ�� */
+	@Override
+	public int idCheck(String userId) throws Exception {
+		
+		return membermapper.idCheck(userId);
+	}
+	
+	/* ��й�ȣ �ߺ� Ȯ�� */
+	@Override
+	public int passCheck(String userPass) throws Exception {
+		
+		return membermapper.passCheck(userPass);
+	}
+	
+	
+	
+	
+	/* �α��� */
+	@Override
+	public MemberVO memberLogin(MemberVO member) throws Exception {
+		return membermapper.memberLogin(member);
+	}
+	
+	
+	/* ȸ�� ���� */
+	
+	
+	@Override
+	public void memberUpdate(MemberVO member) {
+		membermapper.memberUpdate(member);
+	}
+	
+	
+	/*ȸ�� Ż��*/
+	
+	@Override
+	public void memberDelete(MemberVO member) {
+		membermapper.memberDelete(member);
+	}
+	
+	
+
+	
+}
